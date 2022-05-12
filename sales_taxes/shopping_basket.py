5180fbd1379e35de21b340166caa55c15bf2707c
Ehsan Attar <incetona@gmail.com>
2022-05-12 16:39:39 +0200

    tests for ShoppingBasket class

--- sales_taxes/shopping_basket.py
+++ sales_taxes/shopping_basket.py
@@ -4,7 +4,14 @@ from typing import List
 
 @dataclass
 class Item:
-    pass
+    """ class for items added """
+
+    name: str
+    unit_price: float
+    quantity: int
+    imported: bool = False
+    total_price: float = None
+    unit_tax: float = None
 
 
 class ShoppingBasket:
--- sales_taxes/test/test_shopping_basket.py
+++ sales_taxes/test/test_shopping_basket.py
@@ -1,10 +1,25 @@
 import unittest
-from sales_taxes.shopping_basket import ShoppingBasket
+from sales_taxes.shopping_basket import ShoppingBasket, Item
+
 
 class TestShoppingBasket(unittest.TestCase):
     """ ShoppingBasket class tests. """
     def test_shopping_basket_is_empty(self):
         shopping_basket = ShoppingBasket()
+        self.assertEqual(len(shopping_basket.items), 0)
+
+    def test_check_single_item_is_in_shopping_basket(self):
+        shopping_basket = ShoppingBasket()
+        shopping_basket.items.append(Item('book', 12.49, 1))
+        self.assertIn(Item('book', 12.49, 1), shopping_basket.items)
+
+    def test_check_items_are_in_shopping_basket(self):
+        shopping_basket = ShoppingBasket()
+        shopping_basket.items.append(Item('book', 12.49, 1))
+        shopping_basket.items.append(Item('music CD', 16.49, 1))
+
+        self.assertIn(Item('book', 12.49, 1), shopping_basket.items)
+        self.assertIn(Item('music CD', 16.49, 1), shopping_basket.items)
 
 
 if __name__ == '__main__':
